1f20e9bacee42a6d699a591fddcb44fc7a807dfb
unknown <saurav barnwal>
2023-02-03 16:10:55 +0530

    readme file added

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 const express=require('express')
 const { connection } = require('./config/db')
 const { fruitRouter } = require('./routes/fruit.route')
+const cors=require('cors')
 require('dotenv').config()
 const PORT=process.env.PORT || 8080
 const app=express()
+app.use(cors())
+app.use(express.json())
+app.use("/",fruitRouter)
 app.get("/",((req,res)=>{
     res.send("<h1>Welcome to my world <h1/>")
 }))
-app.use(express.json())
-app.use("/",fruitRouter)
 app.listen(PORT,async()=>{
     try {
         await connection
